5b4b107bf9d834c2c49236d32d18355365ef9d6e
PGgamer2 <38359416+PGgamer2@users.noreply.github.com>
2020-06-18 10:01:48 +0200

    Updating download page
    Updated with official and unofficial fork download

--- getLastCommits.js
+++ getLastCommits.js
@@ -5,13 +5,10 @@ xmlhttplc.onreadystatechange = function() {
 		var SerJSONLC = JSON.parse(this.responseText);
 		for (var jsarrayn of SerJSONLC) {
 			if (jsarrayn.name == "master") {
-				document.getElementById("lcm").innerHTML = "Last master branch commit: " + jsarrayn.commit.sha.substring(0, 7);
-			}
-			else if (jsarrayn.name == "nightly") {
-				document.getElementById("lcn").innerHTML = "Last nightly branch commit: " + jsarrayn.commit.sha.substring(0, 7);
+				document.getElementById("lcm").innerHTML = "Last commit: " + jsarrayn.commit.sha.substring(0, 7);
 			}
 		}
 	}
 };
-xmlhttplc.open("GET", "https://api.github.com/repos/sm64pc/sm64pc/branches", true);
+xmlhttplc.open("GET", "https://api.github.com/repos/sm64-port/sm64-port/branches", true);
 xmlhttplc.send();
